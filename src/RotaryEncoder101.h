139942202682adf572c64075f393e8e1a92cc7e8
Coert Vonk <coert.vonk@gmail.com>
2022-02-19 19:35:20 -0800

    Work around big steps in non-linear mode.

--- src/RotaryEncoder101.h
+++ src/RotaryEncoder101.h
@@ -7,47 +7,45 @@ class RotaryEncoderInterruptA;
 class RotaryEncoderInterruptB;
 class RotaryEncoderInterruptP;
 
-class RotaryEncoder
-{
-	friend class RotaryEncoderInterruptA;
-	friend class RotaryEncoderInterruptB;
-	friend class RotaryEncoderInterruptP;
+class RotaryEncoder {
+    friend class RotaryEncoderInterruptA;
+    friend class RotaryEncoderInterruptB;
+    friend class RotaryEncoderInterruptP;
 
   public:
-	  // constructor and begin function  
-	RotaryEncoder(uint8_t const pinA_, uint8_t const pinB_, uint8_t const pinP_, bool const lineair_ = true);
-	void begin(void);
-
-	  // return the current state
-	encoderPos_t getPosition(void);
-	void setPosition(encoderPos_t const pos);
-	bool isPushed(void);
+    RotaryEncoder(uint8_t const pinA_,
+	              uint8_t const pinB_,
+				  uint8_t const pinP_,
+	              bool const linear_ = true);
+    void begin(void);
+    encoderPos_t getPosition(void);
+    void setPosition(encoderPos_t const pos);
+    bool isPushed(void);
+    void reset(void);
 
   private:
-	  // parameters
-	uint8_t const pinP;
-	uint8_t const pinA;
-	uint8_t const pinB;
-	bool const lineair;
-	
-	  // instantiated child classes
-	RotaryEncoderInterruptA * InterruptPtrA;
-	RotaryEncoderInterruptB * InterruptPtrB;
-	RotaryEncoderInterruptP * InterruptPtrP;
+    // parameters
+    uint8_t const pinP;
+    uint8_t const pinA;
+    uint8_t const pinB;
+    bool const linear;
 
-	  // state about what we're expecting
-	bool expectRisingEdgeOnPinA;
-	bool expectRisingEdgeOnPinB;
+    // instantiated child classes
+    RotaryEncoderInterruptA * InterruptPtrA;
+    RotaryEncoderInterruptB * InterruptPtrB;
+    RotaryEncoderInterruptP * InterruptPtrP;
 
-	  // current state
-	volatile bool pushed;
-	volatile encoderPos_t encoderPos = 0;
-	typedef enum { increment, decrement } encoderPosDir_t;
+    // state
+    bool expectRisingEdgeOnPinA;
+    bool expectRisingEdgeOnPinB;
+    volatile bool pushed;
+    volatile encoderPos_t encoderPos = 0;
+    typedef enum { increment, decrement } encoderPosDir_t;
 
-	  // calculates the number of positions to change
-	encoderPos_t delta(encoderPosDir_t const requestedDirection);
-	struct {
-		uint64_t lastTime;
-		encoderPosDir_t direction;
-	} nonLineair;
+    // calculates the number of positions to change
+    encoderPos_t delta(encoderPosDir_t const requestedDirection);
+    struct {
+        uint64_t lastTime;
+        encoderPosDir_t direction;
+    } nonlinear;
 };
